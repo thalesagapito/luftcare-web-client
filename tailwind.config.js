01f72c4c380b2ffe5208774bd685786ec2a446f7
Thales Agapito <thsousa101@gmail.com>
2020-06-06 21:46:17 -0300

    add new focus outlines to buttons and inputs

--- tailwind.config.js
+++ tailwind.config.js
@@ -218,6 +218,8 @@ module.exports = {
       '2xl-center': '0 0 50px -12px rgba(0, 0, 0, 0.25)',
       inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
       outline: '0 0 0 3px rgba(66, 153, 225, 0.5)',
+      'outline-avocado': '0 0 0 3px rgba(58, 175, 162, 0.3)',
+      'outline-danger': '0 0 0 3px rgba(254, 99, 100, 0.3)',
       none: 'none',
     },
     cursor: {
