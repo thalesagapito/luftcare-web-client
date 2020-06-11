6b9566855d3fc3fb04e4fa7a1534acc0f69c3629
Thales Agapito <thsousa101@gmail.com>
2020-06-10 21:39:00 -0300

    remove unused navbar files

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,11 @@
 <template lang="pug">
-.page-wrapper
-  navbar
-  .content
-    nuxt
+  nuxt
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import Navbar from '~/components/organisms/single-instance/navbar/TheNavbar.vue';
 
-export default Vue.extend({
-  components: { Navbar },
-});
+export default Vue.extend({});
 </script>
 
-<style lang="postcss" scoped>
-.page-wrapper {
-  @apply transition-colors duration-300 ease-in-out min-h-screen bg-white text-gray-900;
-}
-</style>
+<style lang="postcss" scoped></style>
