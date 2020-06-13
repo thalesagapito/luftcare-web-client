abc108de2ed1ca8c39e6740f607665451685943b
Thales Agapito <thsousa101@gmail.com>
2020-06-13 14:23:26 -0300

    add underConstruction page

---
+++ components/organisms/single-instance/under-construction/TheUnderConstructionPage.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  .under-construction-wrapper
+    brand-logo.logo
+    .title Versão beta
+    .subtitle Ainda estamos em fase de desenvolvimento. Por isso somente usuários selecionados têm acesso.
+    el-button(
+      @click="$router.push('/auth/login')"
+      round
+    ) Acessar minha conta
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import BrandLogo from '~/components/atoms/BrandLogo.vue';
+
+type Data = {};
+type Methods = {};
+type Computed = {};
+export type Props = {};
+export type Events = {};
+
+export default Vue.extend<Data, Methods, Computed, Props>({
+  components: { BrandLogo },
+});
+</script>
+
+<style lang="postcss" scoped>
+.under-construction-wrapper {
+  @apply w-full h-full bg-avocado flex flex-col items-center justify-center text-white;
+  background-image: linear-gradient(to top right, #3AAFA2 0%, #f9f586 170%);
+
+  .logo {
+    @apply mb-9 h-7;
+  }
+  .title {
+    @apply text-5xl font-bold mb-6;
+  }
+  .subtitle {
+    @apply text-xl text-center max-w-sm mb-10;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
 <template lang="pug">
-  h1 Pagina inicial
+  under-construction
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import UnderConstruction from '~/components/organisms/single-instance/under-construction/TheUnderConstructionPage.vue';
 
-export default Vue.extend({});
+export default Vue.extend({
+  components: { UnderConstruction },
+});
 </script>
 
 <style lang="postcss" scoped>
